b0d4ca9c3a90ea436e247abc818c694c6088c82c
paraggit <pakamble@redhat.com>
2021-07-21 23:38:21 +0530

    Removed some steps

--- jobs/sample_job.groovy
+++ jobs/sample_job.groovy
@@ -1,21 +1,8 @@
-def gitUrl = "https://github.com/codelyrics/bitqueue-core-api.git"
 
 job("MyProject-Build") {
     description "Builds MyProject from master branch."
     parameters {
         stringParam('COMMIT', 'HEAD', 'Commit to build')
-    }
-    scm {
-        git {
-            remote {
-                url gitUrl
-                branch "origin/master"
-            }
-            extensions {
-                wipeOutWorkspace()
-                localBranch master
-            }
-        }
     }
     steps {
         shell "Look: I'm building master!"
